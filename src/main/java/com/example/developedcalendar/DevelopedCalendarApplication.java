fc1dcc0e6a454f937590e98094a211080d447461
116_Lv <lbw0109@gmail.com>
2026-04-16 16:35:59 +0900

    초기 커밋

---
+++ src/main/java/com/example/developedcalendar/DevelopedCalendarApplication.java
@@ -0,0 +1,13 @@
+package com.example.developedcalendar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevelopedCalendarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevelopedCalendarApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/developedcalendar/DevelopedCalendarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.developedcalendar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevelopedCalendarApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
